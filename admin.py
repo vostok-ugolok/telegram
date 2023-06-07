bb1ad6a327ccde125f57480a6358e2c2ea1b22b8
ncinsli <56553344+ncinsli@users.noreply.github.com>
2023-06-08 01:35:43 +0500

    Разделил админа и клиента
    Админ умеет слать оповещения

---
+++ admin.py
@@ -0,0 +1,94 @@
+from threading import Thread
+from telebot import *
+import os
+import requests
+host = "185.246.64.64:4999"
+
+admin_bot = TeleBot(token=os.environ["VOKSTOK_UGOLOK_TOKEN_ADMIN"])
+
+def order_to_string(order):
+    return f"""🛒  Номер #{order['order_id']}:
+
+Создан: {order['creation_time']}
+Клиент: {order['name']}
+{('''Тип заказа: доставка
+Адрес: ''' + order['adress']) if order['adress'] != '' else 'Тип заказа: в кафе'}
+"""
+
+
+@admin_bot.callback_query_handler(func=lambda call: call.data.split()[0] == 'order' and call.message)
+def orders_navigate(call):
+    data = requests.get('http://' + host + f'/order/get').json()
+
+    direction = call.data.split()[1]
+
+    if direction == 'right':
+        left = int(call.data.split()[2])
+        right = left + min(len(data) - left, 4)
+    else:
+        left = int(call.data.split()[2]) - min(int(call.data.split()[2]), 4)
+        right = left + min(len(data) - left, 4)
+
+    response = f"""📫  Заказы {left} - {right}\n"""
+    for order in data[left:right]:
+        response += '\n\n' + order_to_string(order)
+
+    go_left_button = types.InlineKeyboardButton(text="<", callback_data=f"order left {left}")
+    go_right_button = types.InlineKeyboardButton(text=">", callback_data=f"order right {right}")
+
+    if left > 0 and right < len(data):
+        markup = types.InlineKeyboardMarkup().row(go_left_button, go_right_button)
+
+    elif right == len(data):
+        markup = types.InlineKeyboardMarkup().row(go_left_button)
+    
+    elif left == 0:
+        markup = types.InlineKeyboardMarkup().row(go_right_button)
+
+    admin_bot.edit_message_text(response, call.message.chat.id, call.message.message_id, reply_markup=markup)
+
+
+@admin_bot.message_handler(commands=['orders'])
+def orders(message):
+    data = requests.get('http://' + host + f'/order/get').json()
+    left, right = 0, min(4, len(data))
+    response = f"""📫  Заказы  {left} - {right}\n"""
+
+    for order in data[left:right]:
+        response += '\n\n' + order_to_string(order)
+
+    go_left_button = types.InlineKeyboardButton(text="<", callback_data=f"order left {right}")
+    go_right_button = types.InlineKeyboardButton(text=">", callback_data=f"order right {right}")
+    
+    admin_bot.send_message(message.from_user.id, response, reply_markup=types.InlineKeyboardMarkup().row(go_left_button, go_right_button))
+
+@admin_bot.message_handler(commands=['ready'])
+def ready(message):
+    admin_bot.register_next_step_handler(message, got_id)
+    admin_bot.reply_to(message, '🎈  Введите идентификатор заказа')
+
+def got_id(message):
+    order_id = int(message.text.replace('#', ''))
+    print(requests.post('http://' + host + f'/order/state/update?id={order_id}', json={"new_state" : "READY", 'order_id': order_id}).text)
+
+    admin_bot.reply_to(message, '✅  Информация отправлена')
+
+@admin_bot.message_handler(commands=['notify'])
+def notify(message):
+    admin_bot.register_next_step_handler(message, got_id_before_message)
+    admin_bot.reply_to(message, '🎈  Введите идентификатор заказа')
+
+def got_id_before_message(message):
+    order_id = int(message.text.replace('#', ''))
+
+    admin_bot.reply_to(message, '🎈  Введите сообщение')
+
+    admin_bot.register_next_step_handler(message, got_message, order_id)
+
+def got_message(message, order_id):
+    print(requests.post('http://' + host + f'/order/state/update?id={order_id}', json={"new_state" : f"STATE CHANGED AT {datetime.now().strftime('%d.%m.%Y %H:%M')}", 'order_id': order_id, 'message': message.text}).text)
+
+    admin_bot.reply_to(message, '✅  Информация отправлена')
+
+print('⚡  Бот-админ запустился')
+admin_bot.infinity_polling()
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from telebot import *
 from associations import *
 import os
+from threading import Thread
 import requests
 import json
 import socketio
 import socketio.exceptions
 
 host = "185.246.64.64:4999"
-bot = TeleBot(token=os.environ["VOSTOK_UGOLOK_TOKEN"])
-bot.parse_mode = 'html'
+client_bot = TeleBot(token=os.environ["VOSTOK_UGOLOK_TOKEN"])
+client_bot.parse_mode = 'html'
+
 order_to_user = Associations()
 sio = socketio.Client()
 owner = '399445674'
@@ -17,10 +19,10 @@ try:
     sio.connect('ws://' + host)
 
 except socketio.exceptions.ConnectionError:
-    bot.send_message(owner, '🔥  Не удаётся подключиться к серверу! Бот отключён')
+    client_bot.send_message(owner, '🔥  Не удаётся подключиться к серверу! Бот отключён')
     exit()
 
-@bot.message_handler(commands=['start'])
+@client_bot.message_handler(commands=['start'])
 def start(message):
     args = message.text.split()[1:]
     if args != []:
@@ -37,7 +39,7 @@ def start(message):
             order_to_user.assoc(order, message.from_user.id)
             order_to_user.save()
 
-            bot.send_message(message.from_user.id, f"""<b>Добрый день!</b>
+            client_bot.send_message(message.from_user.id, f"""<b>Добрый день!</b>
                              
 📍  Выбран заказ номер <b>{order_id}</b>
 
@@ -49,7 +51,7 @@ def start(message):
 Мы отправим уведомление, когда он будет готов""")
             
         else:
-            bot.send_message(message.from_user.id, f"""Добрый день!
+            client_bot.send_message(message.from_user.id, f"""Добрый день!
 Выбранный Вами заказ под номером {order_id} доставляется через сервис Яндекс.Еда
 Для отслеживания заказа используйте соответствующее приложение""")
 
@@ -63,15 +65,16 @@ def on_order_ready(id_state: list):
         order_addinfo = id_state[2]
 
     if order_state == 'READY':
-        bot.send_message(order_to_user.data[order_id], f'🕒 Ваш заказ готов!\n \nНомер заказа: <b>{order_id}</b>')
+        client_bot.send_message(order_to_user.data[order_id], f'🕒 Ваш заказ готов!\n \nНомер заказа: <b>{order_id}</b>')
     
     elif order_addinfo != None:
-        bot.send_message(order_to_user.data[order_id], f'⚠ Новая информация по Вашему заказу\n \nНомер заказа: <b>{order_id}</b>\n\n{order_addinfo}')
+        client_bot.send_message(order_to_user.data[order_id], f'⚠ Новая информация по Вашему заказу\n \nНомер заказа: <b>{order_id}</b>\n\n{order_addinfo}')
 
 @sio.on('connection')
 def connected():
-    bot.send_message(owner, '✅  Подключение к серверу восстановлено')
+    client_bot.send_message(owner, '✅  Подключение к серверу восстановлено')
 
 if __name__ == '__main__':
-    print('⚡ Бот запущен')
-    bot.infinity_polling()
+    print('⚡  Бот-клиент запустился')
+    
+    client_bot.infinity_polling()
