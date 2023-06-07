adf405eb2f4ab47e548be636a18534dc8b740a0c
ncinsli <56553344+ncinsli@users.noreply.github.com>
2023-06-07 19:26:19 +0500

    Данные о заказе socket.io
    Теперь бот выводит готовые заказы

--- associations.py
+++ associations.py
@@ -4,7 +4,7 @@ import json
 class Associations:
     def __init__(self):
         self.path = 'associations.json'
-        self.data = {}
+        self.data = dict()
         if not os.path.exists('associations.json'):
             self.save()
         else:
@@ -12,11 +12,11 @@ class Associations:
 
     def save(self):
         with open(self.path, 'w') as f:
-            json.dump(self.data)
+            json.dump(self.data, f)
 
     def load(self):
         with open(self.path, 'r') as f:
-            self.data = json.load(self.data)
+            self.data = dict(json.load(f))
 
-    def assoc(self, order: int, id: str):
-        self.data[order] = id
+    def assoc(self, order: dict, id: str):
+        self.data[order["order_id"]] = id
