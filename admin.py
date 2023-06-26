cab0cc4cc6e8a328508f7920e0db1ef5352eccfd
ncinsli <56553344+ncinsli@users.noreply.github.com>
2023-06-27 02:50:14 +0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,6 +2,7 @@ from threading import Thread
 from telebot import *
 import os
 import requests
+import json
 host = "212.109.193.12:5000"
 
 admin_bot = TeleBot(token=os.environ["VOSTOK_UGOLOK_TOKEN_ADMIN"])
@@ -106,12 +107,85 @@ def food_to_string(food):
 def menu(message):
     result = requests.get('http://' + host + '/food/get').json()
 
+    buttons = types.InlineKeyboardMarkup()
     response = '🎁  Меню'
 
-    for food in result:
-        response += '\n\n' + food_to_string(food)
+    for i in range(min(len(result), 3)):
+        buttons.add(types.InlineKeyboardButton(result[i]['name'], callback_data='inspect ' + result[i]['identifier']))
 
-    admin_bot.send_message(message.from_user.id, response)
+    buttons.row(types.InlineKeyboardButton('<', callback_data=f'menu_back {0}'), types.InlineKeyboardButton('>', callback_data=f'menu_forward {0}'))
+
+    admin_bot.send_message(message.from_user.id, response, reply_markup=buttons)
+
+@admin_bot.callback_query_handler(func=lambda call: call.data.split()[0] in ('menu_back', 'menu_forward'))
+def move(callback: types.CallbackQuery):
+    left = int(callback.data.split()[1]) + (3 if callback.data.split()[0] == 'menu_forward' else -3)
+    if left < 0: return
+
+    result = requests.get('http://' + host + '/food/get').json()[left:]
+    if result == []: return
+    buttons = types.InlineKeyboardMarkup()
+
+    for i in range(min(len(result), 3)):
+        buttons.add(types.InlineKeyboardButton(result[i]['name'], callback_data='inspect ' + result[i]['identifier']))
+        
+    buttons.row(types.InlineKeyboardButton('<', callback_data=f'menu_back {left}'), types.InlineKeyboardButton('>', callback_data=f'menu_forward {left}'))
+
+    admin_bot.edit_message_reply_markup(callback.from_user.id, callback.message.id, reply_markup=buttons)
+
+@admin_bot.callback_query_handler(func=lambda call: call.data.split()[0] == 'inspect')
+def inspect(callback):
+    identifier = callback.data.split()[1]
+
+    result = requests.get(f'http://{host}/food/get?type={identifier}').json()[0]
+    image_url = 'http://' + host + '/images/' + result['image']
+    image_response = requests.get(image_url)
+
+    with open("image.jpg", "wb") as f:
+        f.write(image_response.content)
+
+    with open("image.jpg", "rb") as f:
+        admin_bot.send_photo(callback.from_user.id, f, f"""{result['name']}
+
+Описание: {result['description']}
+Цена: {result['price']}
+Масса: {result['mass']}
+Идентификатор: {result['identifier']}
+
+""")
+    os.remove('image.jpg')
+
+@admin_bot.message_handler(commands=['food_add'])
+def food_add(message):
+    args = message.text.split('\n')[1:]
+    if len(args) < 6:
+        admin_bot.reply_to(message, """❌  Неверный формат!
+Имя блюда: Текст
+Цена: Число
+Название картинки: Текст
+Описание: Текст
+Масса: Число
+Идентификатор: Текст в формате category/category/.../eng_name""")
+    name = args[0]
+    price = args[1]
+    image = args[2]
+    description = args[3]
+    mass = args[4]
+    identifier = args[5]
+
+    body = {
+        "name": name,
+        "price": price,
+        "image": image,
+        "description": description,
+        "mass": mass,
+        "avaiable": True,
+        "identifier": identifier
+    }
+    print(json.dumps(body, ensure_ascii=False))
+    result = requests.post('http://' + host + '/food/add', data=json.dumps(body)).text
+    admin_bot.reply_to(message, result)
+
 
 print('⚡  Бот-админ запустился')
 admin_bot.infinity_polling()
